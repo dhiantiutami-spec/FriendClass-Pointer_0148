a00cfd6577fbb051ee26f7520ac633990a1b5560
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-04-28 19:00:31 +0700

    menambah class pelajar

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -5,6 +5,15 @@ class pelajar;
 class manusia
 {
 public:
-    void showNilaipelajar(pelajar &x);
+    void showNilaiPelajar(pelajar &x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
